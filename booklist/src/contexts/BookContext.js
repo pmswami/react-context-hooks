1ec9f296ab63ec779d46ef350cb4311aa91a1317
unknown <usern5949@gmail.com>
2024-01-04 22:59:59 +0530

    added reducers

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,23 +1,32 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useReducer} from 'react';
 import uuid from "react-uuid"
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-    const [ books, setBooks ] = useState([
+    const [books, dispatch] = useReducer(bookReducer, [
         { title: "The Name of The Wind", author:"Patrick Rothfuss", id:1 },
         { title: "The Final Empire",author: "Brandon Sanderson", id:2 },
     ])
-    const addBook = (title, author)=>{
-        setBooks([...books, {title, author, id:uuid()}])
-    }
-    const removeBook = (id)=>{
-        setBooks(books.filter(book=>book.id!==id))
-    }
+
+    // const [ books, setBooks ] = useState([
+    //     { title: "The Name of The Wind", author:"Patrick Rothfuss", id:1 },
+    //     { title: "The Final Empire",author: "Brandon Sanderson", id:2 },
+    // ])
+    // const addBook = (title, author)=>{
+    //     setBooks([...books, {title, author, id:uuid()}])
+    // }
+    // const removeBook = (id)=>{
+    //     setBooks(books.filter(book=>book.id!==id))
+    // }
     return ( 
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
+        // <BookContext.Provider value={{books, addBook, removeBook}}>
+        //     {props.children}
+        // </BookContext.Provider>
      );
 }
  
